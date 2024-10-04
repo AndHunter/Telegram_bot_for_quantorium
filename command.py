b9af15e78832633621c7821185b759360e600ce9
unknown <sergeevanrej7824@gmail.com>
2024-10-04 19:27:15 +0500

    Finished the 'Бесплатные курсы' thread with keyboards for them, TODO

--- command.py
+++ command.py
@@ -1,11 +1,12 @@
 from aiogram.types import Message
-from keyboard import keyboard_start, keyboard_help, keyboard_free_courses, keyboard_how_to_get
-
+from keyboard import keyboard_start, keyboard_help, keyboard_free_courses, keyboard_how_to_get, quantum_keyboard, keyboard_record
+from datetime import datetime
 link_site, text_site = 'https://kvantorium-perm.ru/', "сайте"
 link_site_, text_site_ = 'https://kvantorium-perm.ru/', "сайт"
 link_vk, text_vk = 'https://vk.com/kvantorium.fotonika', 'VK'
 link_youtube, text_youtube = 'https://www.youtube.com/channel/UC8Q99tRVe6T-zzsjBI89RWQ/videos', 'Youtube'
 link_tg, text_tg = 'https://t.me/kvantoriumperm', 'Telegram'
+
 async def first_cmd(message: Message):
     await message.answer('Здравствуйте, вас приветствует помощник по учебному заведению "Кванториум Фотоника".',
                          reply_markup=keyboard_start)
@@ -41,6 +42,22 @@ async def free_courses_cmd(message: Message):
 async def how_to_get_cmd(message: Message):
     await message.answer(f"Вам нужно пройти тесты для вступления, на нашем сайте {f"<a href=\"{link_site}\">{text_site}</a>"}", parse_mode="HTML", reply_markup=keyboard_how_to_get)
 
+async def all_quantuams_cmd(message: Message):
+    await message.answer(f"Выберите квантум о котором хотите узнать подробней ", reply_markup=quantum_keyboard)
+
+async def record_cmd(message: Message):
+    current_month = datetime.now().month
+
+    if current_month in [1, 8, 9, 10]:  # Январь, Август, Сентябрь, Октябрь(для работы) TODO узнать дни записи на курс
+        await message.answer(
+            "Чтобы записаться на курс, пожалуйста, заполните форму.", #TODO получить ссылку на форму
+            reply_markup=keyboard_record
+        )
+    else:
+        await message.answer(
+            "Запись на курсы закончена, следующая начнётся в начале полугодия.",
+            reply_markup=keyboard_record
+        )
 async def handler_command(message: Message):
     if message.text.lower() == "информация":
         await help_cmd(message)
@@ -50,3 +67,7 @@ async def handler_command(message: Message):
         await free_courses_cmd(message)
     elif message.text.lower() == "как попасть":
         await how_to_get_cmd(message)
+    elif message.text.lower() == "все кванториумы":
+        await all_quantuams_cmd(message)
+    elif message.text.lower() == "записаться на курс":
+        await record_cmd(message)
--- keyboard.py
+++ keyboard.py
@@ -2,7 +2,7 @@ from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram import Bot, Dispatcher, types
-
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 kb_start = [
     [
@@ -43,7 +43,7 @@ kb_free_courses = [
 
 ]
 keyboard_free_courses = types.ReplyKeyboardMarkup(
-    keyboard=kb_help,
+    keyboard=kb_free_courses,
     resize_keyboard=True,
     input_field_placeholder=f"Просто нажми на кнопку"
 )
@@ -52,13 +52,47 @@ kb_how_to_get = [
     [
     types.KeyboardButton(text="Все кванториумы"),
     types.KeyboardButton(text="Информация"),
+    types.KeyboardButton(text="Записаться на курс"),
     types.KeyboardButton(text="Назад")
      ]
 
 ]
 keyboard_how_to_get = types.ReplyKeyboardMarkup(
-    keyboard=kb_help,
+    keyboard=kb_how_to_get,
     resize_keyboard=True,
     input_field_placeholder=f"Просто нажми на кнопку"
 )
 
+kb_record = [
+    [
+    types.KeyboardButton(text="Информация"),
+    types.KeyboardButton(text="Назад")
+     ]
+
+]
+keyboard_record = types.ReplyKeyboardMarkup(
+    keyboard=kb_record,
+    resize_keyboard=True,
+    input_field_placeholder=f"Просто нажми на кнопку"
+)
+
+button_media = InlineKeyboardButton(text="Медиаквантум", url="https://kvantorium-perm.ru/quantum/media/")
+button_aero = InlineKeyboardButton(text="Аэроквантум", url="https://kvantorium-perm.ru/quantum/aero/")
+button_bio = InlineKeyboardButton(text="Биоквантум", url="https://kvantorium-perm.ru/quantum/bio/")
+button_cosmo = InlineKeyboardButton(text="Космоквантум", url="https://kvantorium-perm.ru/quantum/cosmo/")
+button_it = InlineKeyboardButton(text="IT-квантум", url="https://kvantorium-perm.ru/quantum/it/")
+button_nano = InlineKeyboardButton(text="Наноквантум", url="https://kvantorium-perm.ru/quantum/nano/")
+button_hightech = InlineKeyboardButton(text="Хайтек", url="https://kvantorium-perm.ru/quantum/hightech/")
+button_photonics = InlineKeyboardButton(text="Фотоника", url="https://kvantorium-perm.ru/quantum/photonics/")
+button_vr_ar = InlineKeyboardButton(text="VR / AR", url="https://kvantorium-perm.ru/quantum/vr/")
+button_design = InlineKeyboardButton(text="Промышленный дизайн", url="https://kvantorium-perm.ru/quantum/promodesign/   ")
+button_robot = InlineKeyboardButton(text="Промробоквантум", url="https://kvantorium-perm.ru/quantum/robo/")
+
+quantum_keyboard = InlineKeyboardMarkup(row_width=2, inline_keyboard=[
+    [button_media, button_aero],
+    [button_bio, button_cosmo],
+    [button_it, button_nano],
+    [button_hightech, button_photonics],
+    [button_vr_ar, button_design],
+    [button_robot]
+])
